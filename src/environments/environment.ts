afd13c0e62f7f73512d7e7fe8de34e4b61d8677a
David Galisteo <david.galisteo@bosonit.com>
2023-10-04 17:31:54 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'gdi-firebase-auth-1b89f',
+    appId: '1:556961775415:web:88ffffd086591b36e0e4b2',
+    storageBucket: 'gdi-firebase-auth-1b89f.appspot.com',
+    apiKey: 'AIzaSyD3an6FSkM766_1KOTuYzVpjo4AinYP7S8',
+    authDomain: 'gdi-firebase-auth-1b89f.firebaseapp.com',
+    messagingSenderId: '556961775415',
+  },
+  production:false
+};
